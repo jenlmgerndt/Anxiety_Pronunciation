d8518d245e94cfd0f36b780785f100199cb93432
JenLMGerndt <jgerndt@purdue.edu>
2012-05-28 23:48:01 -0400

    Excel Setup

--- Preference_Analysis.py
+++ Preference_Analysis.py
@@ -1,6 +1,9 @@
 #  Das Excel-Modul importieren(Infos zu der Benutzung von Excel-Dateien, damit Python mit Excel kompatibel ist.)
 import xlrd
 
-Spring102 = xlrd.open_workbook("Spring_2012_102")
+# Die Arbeitsmappe oeffnen. Arbeitsnmappe (workbook) heisst jetzt Spring102.
+Spring102 = xlrd.open_workbook("Spring_2012_102.xls")
 
-print "Opened it successfully."
+# Das Blatt in der Arbeitsmappe oeffnen.
+sh = Spring102.sheet_by_name(u"102-at-home-survey")
+
